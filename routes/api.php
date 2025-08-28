30a5783964d4d4a98017035568bd2c8032a75264
Youssouf Donzo <Donzoyoussouf@gmail.com>
2025-08-28 16:59:11 +0000

    u

--- routes/api.php
+++ routes/api.php
@@ -6,4 +6,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::resource("produit", ProduitController::class);
 
-Route::post("ajouter-produit", [ProduitController::class, 'store']);
+Route::post("ajouter", [ProduitController::class, 'store']);
